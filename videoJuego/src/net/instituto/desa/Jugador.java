a58ac2ac937ccff4d26bb37237a2c6356e1486af
Inma_Burlo <inmaburlo@gmail.com>
2020-03-09 20:40:28 +0100

    Cambios propuesto en el ejercicio

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -18,5 +18,21 @@ public class Jugador {
 	public void setX(int x) {
 		this.x = x;
 	}
-
+	
+	public void moverDerecha() {
+		this.setX(this.getX() + 1);
+	}
+	
+	public void moverIzquierda() {
+		this.setX(this.getX() - 1);
+	}
+	
+	public void moverArriba() {
+		this.setY(this.getY() - 1);
+	}
+	
+	public void moverAbajo() {
+		this.setY(this.getY() + 1);
+	}
+	
 }
